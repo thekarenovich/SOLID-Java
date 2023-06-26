8882ab6c4d41b4ee75b85bd741e91f4ad80b20cb
Erik Karenovich <78443570+thekarenovich@users.noreply.github.com>
2023-06-26 23:24:17 +0300

    Create incorrect.java

---
+++ Single_responsibility_principle/incorrect.java
@@ -0,0 +1,13 @@
+public class Customer {
+    public void placeOrder() {
+        // Размещение заказа
+    }
+    
+    public void payOrder() {
+        // Оплата заказа
+    }
+    
+    public void sendNotification() {
+        // Отправка уведомления покупателю
+    }
+}
